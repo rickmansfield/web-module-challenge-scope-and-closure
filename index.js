2b4ae5d1865ade98dc4446468eb5e8f1bf47d9d4
Rick <rmansfield@realpeoplesratings.com>
2021-06-14 18:50:49 -0500

    Task 4 complete

--- index.js
+++ index.js
@@ -106,9 +106,16 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
-}
+  function getInningScore(inningcb) {
+    const inningScore = {
+      Home: 0, 
+      Away: 0
+    }
+    inningScore.Home = inningcb();
+    inningScore.Away = inningcb();
+    return inningScore;
+  }
+  console.log('Task 4', getInningScore);
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
